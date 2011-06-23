88e195e992a1c2b21f40d0b80b699b114e750ae6
Kurt Yoder <ktynotorion@yoderhome.com>
2011-06-23 09:28:29 -0400

    Subclass galaxy objects from All
    This allows batch/group assignment to be done within the galaxy object.
    Labels need to have batch/group assigned when they are created, so this reorganization is necessary for this reason.

--- src/galaxy_objects.py
+++ src/galaxy_objects.py
@@ -6,162 +6,17 @@ class MissingDataException(Exception): pass
 class DataError(Exception): pass
 class RangeException(Exception): pass
 
-class BackgroundStar(object):
-	'A simplified star for the background, to be drawn only as a point source'
-
-	def __init__(self, coordinates, color):
-		self.coordinates = (coordinates[0], coordinates[1], 0)
-		self.color = color
-
-class ForegroundObject(object):
-	'All foreground objects that appear in the galaxy window, eg stars, black holes, and nebulae.'
-
-	# all will need to load images from pyglet's resource path
-	pyglet.resource.path = ['../images']
-	pyglet.resource.reindex()
-
-	def __init__(self, coordinates):
-		if not (-10000 < coordinates[0] < 10000) or not (-10000 < coordinates[1] < 10000):
-			raise RangeException, "coordinates must be between -10000 and 10000"
-
-		self.coordinates = coordinates
-
-class ScaledForegroundObject(ForegroundObject):
-	'All foreground objects that maintain a constant size across rescales, eg stars and black holes.'
-
-	def __init__(self, coordinates, image):
-		super(ScaledForegroundObject, self).__init__(coordinates)
-		self.sprite = pyglet.sprite.Sprite(image,
-			x=coordinates[0], y=coordinates[1]
-		)
-		self.sprite_origin = (self.image.width/2, self.image.height/2)
-		self.sprite.image.anchor_x = self.sprite_origin[0]
-		self.sprite.image.anchor_y = self.sprite_origin[1]
-
-		self.sprite.scale = 0.1
-
-		self.scaled_sprite_origin = (self.sprite_origin[0]*self.sprite.scale, self.sprite_origin[1]*self.sprite.scale)
-
-	def scale_coordinates(self, scaling_factor):
-		"Set object's sprite coordinates based on a scaling factor."
-		self.sprite.x = self.coordinates[0]/scaling_factor
-		self.sprite.y = self.coordinates[1]/scaling_factor
-
-class ForegroundStar(ScaledForegroundObject):
-	'A named star and all its properties.'
-	# this is B/W, and will be colored using pyglet's sprite.color
-	image = pyglet.resource.image('star.png')
-
-	color_maps = {
-		'white': (255, 255, 255),
-		'blue': (76, 177, 255),
-		'green': (78, 255, 47),
-		'yellow': (255, 255, 0),
-		'orange': (255, 145, 0),
-		'red': (220, 20, 60),
-		'brown': (70, 22, 0),
-	}
-
-	def __init__(self, coordinates, name, type='yellow'):
-		super(ForegroundStar, self).__init__(coordinates, self.image)
-
-		if (len(name) > 15) or (len(name) < 3):
-			raise RangeException, "name must be between 3 and 15 characters long"
-		self.name = name
-
-		if not self.color_maps.has_key(type):
-			raise DataError, 'unknown star type: %s'%type
-		self.sprite.color = self.color_maps[type]
-
-		self.label = pyglet.text.Label(name,
-			font_name='Arial', font_size=8,
-			# x and y will immediately be recalculated, but are required to initialize the label
-			x=coordinates[0], y=coordinates[1],
-			anchor_x='center', anchor_y='top')
-
-	def scale_coordinates(self, scaling_factor):
-		"Set star's sprite and label coordinates based on a scaling factor."
-		super(ForegroundStar, self).scale_coordinates(scaling_factor)
-
-		# center label under sprite
-		self.label.x = self.sprite.x-self.scaled_sprite_origin[0]+(self.sprite.width/2)
-		self.label.y = self.sprite.y-self.scaled_sprite_origin[1]
-
-class BlackHole(ScaledForegroundObject):
-	image = pyglet.resource.image('black_hole.png')
-
-	def __init__(self, coordinates, initial_rotation=0):
-		super(BlackHole, self).__init__(coordinates, self.image)
-		self.sprite.rotation = initial_rotation
-
-class Nebula(ForegroundObject):
-	# all lobe colors in one nebula center on either red, green, or blue in the color wheel:
-	lobe_colors = {
-		'red': ['pink', 'yellow'],
-		'green': ['cyan', 'yellow'],
-		'blue': ['cyan', 'pink']
-	}
-	lobe_images = {}
-	for primary in lobe_colors.keys():
-		for secondary in lobe_colors[primary]:
-			for sprite_number in [1,2]:
-				sprite_identifier = '%s_%s_%d'%(primary, secondary, sprite_number)
-				lobe_images[sprite_identifier] = pyglet.resource.image(
-					'%s_%s_nebula_%d.png'%(primary, secondary ,sprite_number)
-				)
-	max_coordinate = 200
-	min_scale = 0.25
-	max_scale = 4.0
-
-	def __init__(self, coordinates, color, lobes):
-		if (len(lobes) > 6) or (len(lobes) < 1):
-			raise RangeException, "number of lobes must be between 1 and 6"
-		if not self.lobe_colors.has_key(color):
-			raise DataError, "invalid primary color: %s"%color
-
-		super(Nebula, self).__init__(coordinates)
-		self.primary_color = color
-		self.lobes = []
-		for lobe in lobes:
-			lobe_info = {}
-			if (lobe[0] > 1) or (lobe[0] < 0):
-				raise RangeException, "lobe secondary color must be between 0 and 1"
-			lobe_info['secondary'] = self.lobe_colors[color][lobe[0]]
-			if (lobe[1] > 2) or (lobe[1] < 1):
-				raise RangeException, "lobe sprite identifier must be between 1 and 2"
-			if (lobe[2][0] > self.max_coordinate) or (lobe[2][0] < -self.max_coordinate) or (lobe[2][1] > self.max_coordinate) or (lobe[2][1] < -self.max_coordinate):
-				raise RangeException, "lobe x and y coordinates must fall between %d and %d"%(self.max_coordinate, -self.max_coordinate)
-			lobe_info['coordinates'] = lobe[2]
-			if (lobe[3] > 360) or (lobe[3] < 0):
-				raise RangeException, "lobe rotation must be between 0 and 360"
-			lobe_info['rotation'] = lobe[3]
-			if (lobe[4] > self.max_scale) or (lobe[4] < self.min_scale):
-				raise RangeException, "lobe scale must fall between %0.2f and %0.2f"%(self.min_scale, self.max_scale)
-			lobe_info['scale'] = lobe[4]
-
-			sprite_identifier = '%s_%s_%d'%(self.primary_color, lobe_info['secondary'], lobe[1])
-			image = self.lobe_images[sprite_identifier]
-			lobe_info['sprite'] = pyglet.sprite.Sprite(
-				image,
-				# initial coordinates will never be used
-				x=0, y=0
-			)
-			lobe_info['sprite_origin'] = (image.width/2, image.height/2)
-			lobe_info['sprite'].image.anchor_x = lobe_info['sprite_origin'][0]
-			lobe_info['sprite'].image.anchor_y = lobe_info['sprite_origin'][1]
-			lobe_info['sprite'].rotation = lobe_info['rotation']
-
-			self.lobes.append(lobe_info)
-
-	def scale_coordinates_and_size(self, scaling_factor):
-		"Set each lobe's coordinates and size based on a scaling factor."
-		for lobe in self.lobes:
-			lobe['sprite'].x = (self.coordinates[0]+lobe['coordinates'][0])/scaling_factor
-			lobe['sprite'].y = (self.coordinates[1]+lobe['coordinates'][1])/scaling_factor
-			lobe['sprite'].scale = 1/scaling_factor*lobe['scale']
-
 class All(object):
 	"""All galaxy objects are referenced from this object."""
+	sprites_batch = pyglet.graphics.Batch()
+	#render_groups = {}
+	# python equivalent of ruby's each_with_index?
+	#for group_name in ['nebulae', 'black_holes', 'stars', 'labels']:
+	#	render_groups
+	nebulae_group = pyglet.graphics.OrderedGroup(0)
+	black_holes_group = pyglet.graphics.OrderedGroup(1)
+	stars_group = pyglet.graphics.OrderedGroup(2)
+	labels_group = pyglet.graphics.OrderedGroup(3)
 
 	def __init__(self, named_stars, background_stars, black_holes=[], nebulae=[]):
 		if len(background_stars) < 1:
@@ -170,30 +25,10 @@ class All(object):
 
 		if len(named_stars) < 2:
 			raise MissingDataException, "named_stars must have at least two elements"
-		self.sprites_batch = pyglet.graphics.Batch()
-		self.nebulae_group = pyglet.graphics.OrderedGroup(0)
-		self.black_holes_group = pyglet.graphics.OrderedGroup(1)
-		self.stars_group = pyglet.graphics.OrderedGroup(2)
-		self.labels_group = pyglet.graphics.OrderedGroup(3)
 
 		self.named_stars = named_stars
-		for star in self.named_stars:
-			star.sprite.batch = self.sprites_batch
-			star.sprite.group = self.stars_group
-			# batches don't work with labels?!
-			#star.label.batch = self.sprites_batch
-			#star.label.group = self.labels_group
-
 		self.black_holes = black_holes
-		for black_hole in self.black_holes:
-			black_hole.sprite.batch = self.sprites_batch
-			black_hole.sprite.group = self.black_holes_group
-
 		self.nebulae = nebulae
-		for nebula in self.nebulae:
-			for lobe in nebula.lobes:
-				lobe['sprite'].batch = self.sprites_batch
-				lobe['sprite'].group = self.nebulae_group
 
 		self.scalable_objects = self.named_stars+self.black_holes
 
@@ -257,9 +92,6 @@ class All(object):
 
 		self.sprites_batch.draw()
 
-		for star in self.named_stars:
-			star.label.draw()
-
 	def normalize(self):
 		'Force extreme foreground objects to be equidistant from (0,0)'
 		x_offset = (abs(self.right_bounding_x)-abs(self.left_bounding_x))/2
@@ -291,5 +123,165 @@ class All(object):
 		black_hole_rotation_delta = 18.*dt
 		for black_hole in self.black_holes:
 			black_hole.sprite.rotation -= black_hole_rotation_delta
+
+class BackgroundStar(All):
+	'A simplified star for the background, to be drawn only as a point source'
+
+	def __init__(self, coordinates, color):
+		self.coordinates = (coordinates[0], coordinates[1], 0)
+		self.color = color
+
+class ForegroundObject(All):
+	'All foreground objects that appear in the galaxy window, eg stars, black holes, and nebulae.'
+
+	# all will need to load images from pyglet's resource path
+	pyglet.resource.path = ['../images']
+	pyglet.resource.reindex()
+
+	def __init__(self, coordinates):
+		if not (-10000 < coordinates[0] < 10000) or not (-10000 < coordinates[1] < 10000):
+			raise RangeException, "coordinates must be between -10000 and 10000"
+
+		self.coordinates = coordinates
+
+class ScaledForegroundObject(ForegroundObject):
+	'All foreground objects that maintain a constant size across rescales, eg stars and black holes.'
+
+	def __init__(self, coordinates, image, group=None):
+		super(ScaledForegroundObject, self).__init__(coordinates)
+		self.sprite = pyglet.sprite.Sprite(image,
+			x=coordinates[0], y=coordinates[1]
+		)
+		self.sprite_origin = (self.image.width/2, self.image.height/2)
+		self.sprite.image.anchor_x = self.sprite_origin[0]
+		self.sprite.image.anchor_y = self.sprite_origin[1]
+		self.sprite.batch = self.sprites_batch
+		if group:
+			self.sprite.group = group
+
+		self.sprite.scale = 0.1
+
+		self.scaled_sprite_origin = (self.sprite_origin[0]*self.sprite.scale, self.sprite_origin[1]*self.sprite.scale)
+
+	def scale_coordinates(self, scaling_factor):
+		"Set object's sprite coordinates based on a scaling factor."
+		self.sprite.x = self.coordinates[0]/scaling_factor
+		self.sprite.y = self.coordinates[1]/scaling_factor
+
+class ForegroundStar(ScaledForegroundObject):
+	'A named star and all its properties.'
+	# this is B/W, and will be colored using pyglet's sprite.color
+	image = pyglet.resource.image('star.png')
+
+	color_maps = {
+		'white': (255, 255, 255),
+		'blue': (76, 177, 255),
+		'green': (78, 255, 47),
+		'yellow': (255, 255, 0),
+		'orange': (255, 145, 0),
+		'red': (220, 20, 60),
+		'brown': (70, 22, 0),
+	}
+
+	def __init__(self, coordinates, name, type='yellow'):
+		super(ForegroundStar, self).__init__(coordinates, self.image, self.stars_group)
+
+		if (len(name) > 15) or (len(name) < 3):
+			raise RangeException, "name must be between 3 and 15 characters long"
+		self.name = name
+
+		if not self.color_maps.has_key(type):
+			raise DataError, 'unknown star type: %s'%type
+		self.sprite.color = self.color_maps[type]
+
+		self.label = pyglet.text.Label(name,
+			font_name='Arial', font_size=8,
+			# x and y will immediately be recalculated, but are required to initialize the label
+			x=coordinates[0], y=coordinates[1],
+			anchor_x='center', anchor_y='top', 
+			batch=self.sprites_batch, group=self.labels_group)
+
+	def scale_coordinates(self, scaling_factor):
+		"Set star's sprite and label coordinates based on a scaling factor."
+		super(ForegroundStar, self).scale_coordinates(scaling_factor)
+
+		# center label under sprite
+		self.label.x = self.sprite.x-self.scaled_sprite_origin[0]+(self.sprite.width/2)
+		self.label.y = self.sprite.y-self.scaled_sprite_origin[1]
+
+class BlackHole(ScaledForegroundObject):
+	image = pyglet.resource.image('black_hole.png')
+
+	def __init__(self, coordinates, initial_rotation=0):
+		super(BlackHole, self).__init__(coordinates, self.image, self.black_holes_group)
+		self.sprite.rotation = initial_rotation
+
+class Nebula(ForegroundObject):
+	# all lobe colors in one nebula center on either red, green, or blue in the color wheel:
+	lobe_colors = {
+		'red': ['pink', 'yellow'],
+		'green': ['cyan', 'yellow'],
+		'blue': ['cyan', 'pink']
+	}
+	lobe_images = {}
+	for primary in lobe_colors.keys():
+		for secondary in lobe_colors[primary]:
+			for sprite_number in [1,2]:
+				sprite_identifier = '%s_%s_%d'%(primary, secondary, sprite_number)
+				lobe_images[sprite_identifier] = pyglet.resource.image(
+					'%s_%s_nebula_%d.png'%(primary, secondary ,sprite_number)
+				)
+	max_offset = 200
+	min_scale = 0.25
+	max_scale = 4.0
+
+	def __init__(self, coordinates, color, lobes):
+		if (len(lobes) > 6) or (len(lobes) < 1):
+			raise RangeException, "number of lobes must be between 1 and 6"
+		if not self.lobe_colors.has_key(color):
+			raise DataError, "invalid primary color: %s"%color
+
+		super(Nebula, self).__init__(coordinates)
+		self.primary_color = color
+		self.lobes = []
+		for lobe in lobes:
+			lobe_info = {}
+			if (lobe[0] > 1) or (lobe[0] < 0):
+				raise RangeException, "lobe secondary color must be between 0 and 1"
+			lobe_info['secondary'] = self.lobe_colors[color][lobe[0]]
+			if (lobe[1] > 2) or (lobe[1] < 1):
+				raise RangeException, "lobe sprite identifier must be between 1 and 2"
+			if (lobe[2][0] > self.max_offset) or (lobe[2][0] < -self.max_offset) or (lobe[2][1] > self.max_offset) or (lobe[2][1] < -self.max_offset):
+				raise RangeException, "lobe x and y coordinates must fall between %d and %d"%(self.max_offset, -self.max_offset)
+			lobe_info['coordinates'] = lobe[2]
+			if (lobe[3] > 360) or (lobe[3] < 0):
+				raise RangeException, "lobe rotation must be between 0 and 360"
+			lobe_info['rotation'] = lobe[3]
+			if (lobe[4] > self.max_scale) or (lobe[4] < self.min_scale):
+				raise RangeException, "lobe scale must fall between %0.2f and %0.2f"%(self.min_scale, self.max_scale)
+			lobe_info['scale'] = lobe[4]
+
+			sprite_identifier = '%s_%s_%d'%(self.primary_color, lobe_info['secondary'], lobe[1])
+			image = self.lobe_images[sprite_identifier]
+			lobe_info['sprite'] = pyglet.sprite.Sprite(
+				image,
+				# initial coordinates will never be used
+				x=0, y=0
+			)
+			lobe_info['sprite_origin'] = (image.width/2, image.height/2)
+			lobe_info['sprite'].image.anchor_x = lobe_info['sprite_origin'][0]
+			lobe_info['sprite'].image.anchor_y = lobe_info['sprite_origin'][1]
+			lobe_info['sprite'].rotation = lobe_info['rotation']
+			lobe_info['sprite'].batch = self.sprites_batch
+			lobe_info['sprite'].group = self.nebulae_group
+
+			self.lobes.append(lobe_info)
+
+	def scale_coordinates_and_size(self, scaling_factor):
+		"Set each lobe's coordinates and size based on a scaling factor."
+		for lobe in self.lobes:
+			lobe['sprite'].x = (self.coordinates[0]+lobe['coordinates'][0])/scaling_factor
+			lobe['sprite'].y = (self.coordinates[1]+lobe['coordinates'][1])/scaling_factor
+			lobe['sprite'].scale = 1/scaling_factor*lobe['scale']
 
 # doesn't make sense to call this standalone, so no __main__
