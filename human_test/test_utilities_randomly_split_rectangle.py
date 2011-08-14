5fcc9403dc110a4378ecaea38c4ca60489f69045
Kurt Yoder <ktynotorion@yoderhome.com>
2011-08-14 16:01:57 -0400

    rectangle split works, but recursion now has errors

---
+++ human_test/test_utilities_randomly_split_rectangle.py
@@ -0,0 +1,66 @@
+import pyglet
+from pyglet.gl import *
+import sys
+sys.path.append('../src')
+import utilities
+import random
+
+window = pyglet.window.Window()
+
+(bottom, left, top, right) = (
+	random.randint(-80, -30), 
+	random.randint(-80, -30), 
+	random.randint(30, 80), 
+	random.randint(30, 80), 
+)
+print ('bottom, left, top, right: ',(bottom, left, top, right))
+min_width = random.randint(2,20)
+amount = random.randint(2,20)
+print 'min_width: %d; amount: %d'%(min_width, amount)
+
+layer1 = pyglet.graphics.OrderedGroup(0)
+layer2 = pyglet.graphics.OrderedGroup(1)
+layer3 = pyglet.graphics.OrderedGroup(2)
+layer4 = pyglet.graphics.OrderedGroup(3)
+
+batch1 = pyglet.graphics.Batch()
+# outline of box to be divided
+batch1.add(4, GL_LINE_LOOP, layer1,
+	( 'v2i', (left-1,bottom-1, left-1,top+1, right+1,top+1, right+1,bottom-1)),
+	( 'c3B', (128,128,128, 128,128,128, 128,128,128, 128,128,128))
+)
+
+sub_rect_amounts = utilities.randomly_split_rectangle(bottom,left,top,right,min_width,amount)
+print ('sub_rect_amounts: ',sub_rect_amounts)
+for index, rect in enumerate(sub_rect_amounts.keys()):
+	r = 256
+	g = 128
+	layer = layer2
+	if index:
+		r = 128
+		g = 256
+		layer = layer3
+	(sub_bottom, sub_left, sub_top, sub_right) = rect
+	# sub rectangles
+	batch1.add(4, GL_LINE_LOOP, layer,
+		( 'v2i', (sub_left,sub_bottom, sub_left,sub_top, sub_right,sub_top, sub_right,sub_bottom)),
+		( 'c3B', (r,g,128, r,g,128, r,g,128, r,g,128))
+	)
+
+# base pixel + margin
+batch2 = pyglet.graphics.Batch()
+batch2.add(4, GL_QUADS, layer4,
+	( 'v2i', (left,bottom, left,bottom+min_width-1, left+min_width-1,bottom+min_width-1, left+min_width-1,bottom)),
+)
+
+@window.event
+def on_draw():
+	window.clear()
+	glMatrixMode(GL_PROJECTION)
+	glLoadIdentity()
+	gluOrtho2D(-320, 320, -240, 240)
+	glMatrixMode(GL_MODELVIEW)
+	batch1.draw()
+	batch2.draw()
+
+pyglet.app.run()
--- src/utilities.py
+++ src/utilities.py
@@ -1,10 +1,17 @@
 #! python -O
 # a set of utilities, which should be useful without any associated objects
+from __future__ import division
 import random
+import math
+import sys
+import math
 
 class RangeException(Exception): pass
 
-def random_dispersed_coordinates(bottom=-1000, left=-1000, top=1000, right=1000, amount=500, dispersion=1, seed=None, existing=None):
+global nest_level
+nest_level = 0
+
+def random_dispersed_coordinates(bottom=-1000, left=-1000, top=1000, right=1000, amount=500, dispersion=1, seed=None):
 	'within given min/max limits, generate a given number of coordinates, guaranteed to be a minimum distance apart'
 
 	if top <= bottom:
@@ -12,55 +19,102 @@ def random_dispersed_coordinates(bottom=-1000, left=-1000, top=1000, right=1000,
 	if right <= left:
 		raise RangeException, 'right coordinate must be greater than left coordinate'
 
-	# never allow retries to exceed the number of available placement coordinates
-	# permissible retries starts with the full number of available coordinates
-	# and decrements for every used coordinate
-	# actually this is not an accurate way to define all remaining available coordinates
-	# so we're sacrifice accuracy in favor of simplicity
-	x_span = right-left+1
-	y_span = top-bottom+1
-	permissible_retries = x_span * y_span
-	if (amount*dispersion) > permissible_retries:
-		raise RangeException, 'given dispersed amount can not fit within given space'
-
 	# passing in a seed is useful for testing, but likely nothing else
 	random.seed(seed)
 
-	object_coordinates = {}
-	if existing:
-		all_used_coordinates = existing
+	rectangles = []
+	minimum_length = dispersion+1
+	recurse_into_rectangle(bottom, left, top, right, amount, minimum_length, rectangles)
+	print len(rectangles)
+
+	object_coordinates = []
+	#return object_coordinates
+
+def recurse_into_rectangle(bottom, left, top, right, coordinate_count, minimum_length, rectangles):
+	'''recursively split a rectangle into subrectangles
+	allow sufficient area within each subrectangle for the specified minimum_length
+		if each coordinate has margin 1, the minimum_length is 2, and the area is 4
+			a margin of 2 has minimum_length 3 and area 9, etc
+	continue dividing/recursing until each rectangle contains exactly one coordinate'''
+
+	if coordinate_count == 0:
+		raise Exception, "invalid coordinate_count: 0"
+
+	if coordinate_count == 1:
+		rectangles.append((bottom, left, top, right))
+		return
+	
+	for sub_rectangle, sub_coordinate_count in randomly_split_rectangle(bottom, left, top, right, minimum_length, coordinate_count).iteritems():
+		recurse_into_rectangle(
+			sub_rectangle[0], sub_rectangle[1], sub_rectangle[2], sub_rectangle[3], 
+			sub_coordinate_count, minimum_length, rectangles
+		)
+
+def randomly_split_rectangle(bottom, left, top, right, minimum_length, coordinate_count):
+	'''partition a rectangle into two sub-rectangles
+	such that each has sufficient area to hold "coordinate_count"
+	with area also left over for margins/"minimum_length"'''
+	minimum_area = minimum_length**2 
+	coordinate_count1 = int(math.ceil(coordinate_count/2)) 
+
+	# randomly reassign odd remainder (if any) from first half to second half
+	if random.randint(0,1) and (coordinate_count1 > 1) and (coordinate_count%2):
+		coordinate_count1 -= 1
+
+	coordinate_count2 = coordinate_count - coordinate_count1 
+	minimum_split_area1 = coordinate_count1*minimum_area 
+	minimum_split_area2 = coordinate_count2*minimum_area 
+
+	x_span = right-left+1 
+	x_chunks = int(x_span/minimum_length)
+	x_remainder = x_span-(x_chunks*minimum_length)
+
+	y_span = top-bottom+1 
+	y_chunks = int(y_span/minimum_length)
+	y_remainder = y_span-(y_chunks*minimum_length)
+
+	chunk_area = x_chunks*y_chunks
+
+	split_rectangles = []
+
+	# split vertically
+	if x_span > y_span:
+		minimum_left_x_chunks = int(math.ceil(coordinate_count1/y_chunks))
+		minimum_right_x_chunks = int(math.ceil(coordinate_count2/y_chunks))
+		remaining_x_chunks = x_chunks-minimum_left_x_chunks-minimum_right_x_chunks
+
+		if remaining_x_chunks < 0:
+			raise Exception, 'insufficient area to hold coordinates of given minimum width'
+
+		random_x_chunk_offset = random.randint(0, remaining_x_chunks) 
+		random_x_remainder_offset = random.randint(0, x_remainder)
+
+		split_left = left + (minimum_left_x_chunks+random_x_chunk_offset)*minimum_length + random_x_remainder_offset
+		split_right = split_left + 1
+
+		part_counts = {
+			(bottom, left,        top, split_left): coordinate_count1,
+			(bottom, split_right, top, right     ): coordinate_count2,
+		}
+
+	# split horizontally
 	else:
-		all_used_coordinates = {}
+		minimum_bottom_y_chunks = int(math.ceil(coordinate_count1/x_chunks))
+		minimum_top_y_chunks = int(math.ceil(coordinate_count2/x_chunks))
+		remaining_y_chunks = y_chunks-minimum_bottom_y_chunks-minimum_top_y_chunks
 
-	retry_vector = (random.randint(int(left/10), int(right/10)),random.randint(int(bottom/10), int(top/10)))
-	for i in range(amount):
-		random_coordinate = (random.randint(left, right), random.randint(bottom, top))
-		first_coordinate = random_coordinate
-		retries = 0
-		while all_used_coordinates.has_key(random_coordinate):
-			random_coordinate = [random_coordinate[0]+retry_vector[0], random_coordinate[1]+retry_vector[1]]
-			if random_coordinate[0] > right:
-				random_coordinate[0] -= x_span
-			if random_coordinate[0] < left:
-				random_coordinate[0] += x_span
-			if random_coordinate[1] > top:
-				random_coordinate[1] -= y_span
-			if random_coordinate[1] < bottom:
-				random_coordinate[1] += y_span
-			random_coordinate = (random_coordinate[0], random_coordinate[1])
-			if (random_coordinate == first_coordinate):
-				if retries > permissible_retries:
-					raise Exception, 'ran out of available placement coordinates'
-				# retry with a different random vector
-				retry_vector = (random.randint(int(left/10), int(right/10)),random.randint(int(bottom/10), int(top/10)))
-		chosen = random_coordinate
+		if remaining_y_chunks < 0:
+			raise Exception, 'insufficient area to hold coordinates of given minimum height'
 
-		# disallow use of neighbors, out to dispersion distance
-		for x in range(chosen[0]-dispersion+1,chosen[0]+dispersion):
-			for y in range(chosen[1]-dispersion+1,chosen[1]+dispersion):
-				if not all_used_coordinates.has_key((x,y)):
-					all_used_coordinates[(x,y)] = True
-					permissible_retries -= 1
-		object_coordinates[chosen] = True
+		random_y_chunk_offset = random.randint(0, remaining_y_chunks) 
+		random_y_remainder_offset = random.randint(0, y_remainder)
 
-	return (object_coordinates.keys(), all_used_coordinates)
+		split_bottom = bottom + (minimum_bottom_y_chunks+random_y_chunk_offset)*minimum_length + random_y_remainder_offset
+		split_top = split_bottom + 1
+
+		part_counts = {
+			(bottom,    left, split_bottom, right): coordinate_count1,
+			(split_top, left, top,          right): coordinate_count2,
+		}
+
+	return part_counts
