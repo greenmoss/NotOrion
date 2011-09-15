80cbef0b6bb3e8d47169ea448a7eef93af1cca66
Kurt Yoder <ktynotorion@yoderhome.com>
2011-09-15 12:29:10 -0400

    adapt initial setup to new coordinate utility

--- src/setup.py
+++ src/setup.py
@@ -48,11 +48,13 @@ class Choose(object):
 	def generate_galaxy_objects(self):
 		'Generate foreground/background stars, black holes, and nebulae'
 		foreground_limits = (-1500,-1500,1500,1500)
-		foreground_dispersion=100
+		foreground_dispersion = 175
+		foreground_star_count = 50
+		black_hole_count = random.randint(int(foreground_star_count/10), int(foreground_star_count)/5)
 
 		# randomly generate background stars
 		background_stars = []
-		for coordinate in utilities.random_dispersed_coordinates(amount=8000, dispersion=3)[0]:
+		for coordinate in utilities.random_dispersed_coordinates(amount=8000, dispersion=3):
 			color = []
 			for index in range(0,3):
 				color.append(64)
@@ -63,18 +65,27 @@ class Choose(object):
 				galaxy_objects.BackgroundStar(coordinate, color),
 			)
 
+		# generate coordinates for foreground stars AND black holes in one pass
+		# since they may not overlap
+		star_coordinates = utilities.random_dispersed_coordinates(
+			foreground_limits[0], foreground_limits[1], foreground_limits[2], foreground_limits[3],
+			amount=foreground_star_count+black_hole_count, dispersion=foreground_dispersion
+		)
+		# randomly pull coordinates out as black holes
+		black_hole_coordinates = []
+		for repeated in range(black_hole_count):
+			black_hole_coordinates.append( 
+				star_coordinates.pop(random.randint(0,len(star_coordinates)-1))
+			)
+
 		# randomly generate foreground stars
 		foreground_stars = []
 		available_colors = galaxy_objects.ForegroundStar.colors.keys()
-		(foreground_star_coordinates, used_coordinates) = utilities.random_dispersed_coordinates(
-			foreground_limits[0], foreground_limits[1], foreground_limits[2], foreground_limits[3],
-			amount=50, dispersion=foreground_dispersion
-		)
 		available_star_names = []
 		with open('resources/star_names.txt') as star_names_file:
 			for line in star_names_file:
 				available_star_names.append(line.rstrip())
-		for coordinate in foreground_star_coordinates:
+		for coordinate in star_coordinates:
 			foreground_stars.append(
 				galaxy_objects.ForegroundStar(
 					coordinate, 
@@ -85,12 +96,7 @@ class Choose(object):
 
 		# generate a variable number of black holes, minimum distance from foreground stars
 		black_holes = []
-		for coordinate in utilities.random_dispersed_coordinates(
-			foreground_limits[0], foreground_limits[1], foreground_limits[2], foreground_limits[3],
-			amount=random.randint(int(len(foreground_stars)/10), int(len(foreground_stars)/5)),
-			dispersion=foreground_dispersion,
-			existing=used_coordinates
-		)[0]:
+		for coordinate in black_hole_coordinates:
 			black_holes.append(
 				galaxy_objects.BlackHole(coordinate, initial_rotation=random.randint(0,359))
 			)
@@ -98,27 +104,48 @@ class Choose(object):
 		# generate nebulae
 		nebulae = []
 		nebula_colors = galaxy_objects.Nebula.lobe_colors.keys()
-		nebula_offset = galaxy_objects.Nebula.max_offset
+		nebula_color_index = random.randint(0, len(nebula_colors)-1)
+		nebula_offset = 80
+		min_lobes = 3
+		max_lobes = galaxy_objects.Nebula.max_lobes
+
+		# minimize repeated lobe secondary/sprite combinations
+		nebula_secondary_sprite_permutations = [
+			(0, 1), (0, 2), (1, 1), (1, 2)
+		]
+		nebula_permutations_index = random.randint(0,3)
+
 		for coordinate in utilities.random_dispersed_coordinates(
 			foreground_limits[0], foreground_limits[1], foreground_limits[2], foreground_limits[3],
 			amount=random.randint(3,6),
 			dispersion=galaxy_objects.Nebula.max_offset*2
-		)[0]:
-			color = nebula_colors[random.randint(0, len(nebula_colors)-1)]
-			lobe_count = random.randint(galaxy_objects.Nebula.min_lobes, galaxy_objects.Nebula.max_lobes)
+		):
+			color = nebula_colors[nebula_color_index]
+			# cycle through all nebula colors
+			nebula_color_index -= 1
+			if nebula_color_index < 0:
+				nebula_color_index = len(nebula_colors)-1
+
+			lobe_count = random.randint(min_lobes, max_lobes)
 			lobes = []
 			for lobe_coordinate in utilities.random_dispersed_coordinates(
 				-nebula_offset, -nebula_offset, nebula_offset, nebula_offset,
 				amount = lobe_count,
-				dispersion = 20
-			)[0]:
+				dispersion = 15
+			):
+				# cycle through all lobe secondary/sprite combinations
+				secondary_sprite = nebula_secondary_sprite_permutations[nebula_permutations_index]
+				nebula_permutations_index -= 1
+				if nebula_permutations_index < 0:
+					nebula_permutations_index = 3
+
 				lobes.append(
 					(
 						random.randint(0,1),
 						random.randint(1,2),
 						lobe_coordinate,
 						random.randint(0,359),
-						# using exponentiation to ensure floats less than 1.0 are as common as floats greater than 1.0
+						# use exponentiation to ensure floats less than 1.0 are as common as floats greater than 1.0
 						10**random.uniform(-0.3, 0.3)
 					)
 				)
