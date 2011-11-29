1748658732516f9b05908eccf1663e87f2974de7
Kurt Yoder <ktynotorion@yoderhome.com>
2011-11-29 08:01:10 -0800

    Saved game file to OS-appropriate location

--- src/application.py
+++ src/application.py
@@ -8,6 +8,7 @@ import imp
 import sys
 import setup
 import os
+import logging
 
 def set_paths():
 	paths = { 'application': os.path.abspath( __file__ ) }
@@ -15,10 +16,13 @@ def set_paths():
 	paths['root_dir'] = os.path.abspath(os.path.join(paths['code_dir'], os.path.pardir))
 	paths['resources_dir'] = os.path.abspath(os.path.join(paths['root_dir'], 'resources'))
 	paths['images_dir'] = os.path.abspath(os.path.join(paths['resources_dir'], 'images'))
+	paths['preferences_dir'] = pyglet.resource.get_settings_path('NotOrion')
+	paths['saved_games_dir'] = os.path.join(paths['preferences_dir'], 'saved_games')
 
 	# load images using pyglet's resource path
-	pyglet.resource.path = [paths['images_dir']]
-	pyglet.resource.reindex()
+	if (pyglet.resource.path.count(paths['images_dir']) == 0):
+		pyglet.resource.path.append(paths['images_dir'])
+		pyglet.resource.reindex()
 
 	return paths
 
@@ -33,37 +37,31 @@ class Application(object):
 	"Controller class for all game objects and functionality."
 
 	def __init__(self, data=None):
-		self.game_file_path = "/tmp/notorion.save" # temporary, until we find OS-appropriate location
 
-		parser = argparse.ArgumentParser(description='NotOrion')
-		parser.add_argument('--save-game-file')
-		parser.add_argument('--data-init-file')
-		args = parser.parse_args()
+		self.handle_cli_args()
 
-		if data:
-			# allow data to be passed as parameter, mostly useful for testing
-			self.data = data
+		paths = set_paths()
 
-		elif args.data_init_file:
-			# read a python file to initialize self.data
-			# *not* using file handle invocation, since it drops random "c" files
-			imported = imp.load_source('', args.data_init_file)
-			self.data = imported.data
+		if not os.path.exists(paths['preferences_dir']):
+			os.makedirs(paths['preferences_dir'])
 
-		elif args.save_game_file:
-			# unpickle a pickled file as source of self.data
-			with open(args.save_game_file) as save_game_file:
-				set_paths()
-				self.data = pickle.load(save_game_file)
+		if not os.path.exists(paths['saved_games_dir']):
+			os.makedirs(paths['saved_games_dir'])
+		self.game_file_path = os.path.abspath(
+			os.path.join(paths['saved_games_dir'], 'game.pickled')
+		) # right now we only save one game file
 
+		self.try_data_import(data)
+
+		# if data has been loaded, use it
 		if hasattr(self, 'data'):
-			self.data.paths = set_paths()
+			self.data.paths = paths
 			galaxy_window = galaxy.Window(self.data)
 
-		# if we do not yet have game data, we need to generate a new galaxy
+		# otherwise generate a new galaxy
 		else:
 			self.data = DataContainer()
-			self.data.paths = set_paths()
+			self.data.paths = paths
 			setup_window = setup.Choose(self.data)
 
 		pyglet.app.run()
@@ -71,8 +69,53 @@ class Application(object):
 		if hasattr(self, 'data'):
 			self.save()
 
+	def handle_cli_args(self):
+		"Configure and parse command-line arguments"
+		parser = argparse.ArgumentParser(description='NotOrion')
+		parser.add_argument('--save-game-file')
+		parser.add_argument('--data-init-file')
+		parser.add_argument('--continue', action='store_true')
+		self.args = parser.parse_args()
+
+	def try_data_import(self, data):
+		"Determine source of data object, and attempt to import the data"
+		# allow data to be passed as parameter, mostly useful for testing
+		if data:
+			self.data = data
+
+		# read a python file to initialize self.data
+		elif self.args.data_init_file:
+			# *not* using file handle invocation, since it drops random "c" files
+			if os.path.exists(self.args.data_init_file):
+				imported = imp.load_source('', self.args.data_init_file)
+				self.data = imported.data
+			else:
+				logging.warning( "file not found: %s; starting new game"%self.args.data_init_file )
+
+		# unpickle a pickled file as source of self.data
+		elif self.args.save_game_file:
+			if os.path.exists(self.args.save_game_file):
+				with open(self.args.save_game_file) as save_game_file:
+					self.data = pickle.load(save_game_file)
+			else:
+				logging.warning( "file not found: %s; starting new game"%self.args.save_game_file )
+
+		# unpickle the standard game file as source of self.data
+		elif vars(self.args)['continue']: # "self.args.continue" is a syntax error, thus "vars(self.args)" instead
+			if os.path.exists(self.game_file_path):
+				with open(self.game_file_path) as save_game_file:
+					self.data = pickle.load(save_game_file)
+			else:
+				logging.warning( "file not found: %s; starting new game"%self.game_file_path )
+
 	def save(self):
 		'Save game state'
+
+		# do not save unless data container has been populated
+		if not self.data.galaxy_objects:
+			logging.warning( "no data; not saving" )
+			return None
+
 		with open(self.game_file_path, 'w') as save_game_file:
 			pickle.dump(self.data, save_game_file)
 
