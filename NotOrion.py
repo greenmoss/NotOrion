4819c7d5794fbb581694fcf2372adb45cb718ae1
Kurt Yoder <ktynotorion@yoderhome.com>
2012-10-23 20:34:14 -0400

    rename to "NotOrion.py"
    Also move up a level, to be friendly to setup.py.

--- NotOrion.py
+++ NotOrion.py
@@ -4,6 +4,16 @@ should occur elsewhere.
 
 Importantly, the single game-wide global object is initialized *first* so it
 can be reused across the rest of the game.  """
+import os
+import sys
+
+sys.path.append(
+	os.path.join(
+		os.path.dirname(os.path.abspath(__file__)),
+		'src'
+	)
+)
+
 from globals import g
 
 import application
