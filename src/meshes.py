b0b10d658cd874895fb58530e619b9f7d2555c2f
Kurt Yoder <ktynotorion@yoderhome.com>
2013-07-01 09:54:03 -0400

    Split wavefront code into new module
    New module: https://github.com/greenmoss/PyWavefront

--- src/meshes.py
+++ src/meshes.py
@@ -1,30 +1,30 @@
 """This manages all 3D meshes in the game."""
-import os
-import wavefront
-
 import logging
 logger = logging.getLogger(__name__)
+
+import pywavefront
+
 from globals import g
 
 class Mesh(object):
     """This is a wrapper for Wavefront-imported files, adapted for use
     within NotOrion."""
     def __init__(self, object_file_name):
-        path = os.path.join(g.paths['meshes_dir'], object_file_name)
-        imported = wavefront.Wavefront(path)
+        imported = pywavefront.Wavefront(object_file_name)
 
         mesh_count = len(imported.mesh_list)
         if not mesh_count == 1:
             raise Exception, \
                     "found %d meshes within %s; we require exactly one mesh"%(
-                    mesh_count, path)
+                    mesh_count, object_file_name)
         self.mesh = imported.mesh_list[0]
         #logger.debug( self.mesh )
 
         material_count = len(self.mesh.materials)
         if material_count < 1:
             raise Exception, \
-                    "found 0 materials within %s; we require at least one material"%path
+                    "found 0 materials within %s; we require at least one material"%( 
+                    object_file_name)
         #logger.debug( self.mesh.materials[0] )
 
     def set_texture(self, image_path):
